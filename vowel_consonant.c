e1518835416c1b94ba37118c3dc609395c44f8bb
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2025-02-06 21:00:33 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,47 @@
+// Description: . Write a C Program to check Whether a Character is Vowel or Consonant.
+// Author: Angelina Rose Varghese
+// Date: 06/02/2025
+#include <stdio.h>
+int main() {
+    char letter;
+  printf("Enter the Character: ");
+  scanf(" %c",&letter);
+  switch (letter)
+  {
+    case 'a':
+    printf("It is a vowel");
+    break;
+    case 'e':
+    printf("It is a vowel");
+    break;
+    case 'i':
+    printf("It is a vowel");
+    break;
+    case 'o':
+    printf("It is a vowel");
+    break;
+    case 'u':
+    printf("It is a vowel");
+    break;
+    case 'A':
+    printf("It is a vowel");
+    break;
+    case 'E':
+    printf("It is a vowel");
+    break;
+    case 'I':
+    printf("It is a vowel");
+    break;
+    case 'O':
+    printf("It is a vowel");
+    break;
+    case 'U':
+    printf("It is a vowel");
+    break;
+    default:
+    printf(" Consonant");
+    break;
+  }
+
+    return 0;
+}
