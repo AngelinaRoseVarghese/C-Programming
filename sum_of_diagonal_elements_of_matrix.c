60be59f1942317f8834e16ce1a8854ca650a9ad6
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2025-02-17 21:24:59 +0530

    Create sum_of_diagonal_elements_of_matrix.c

---
+++ sum_of_diagonal_elements_of_matrix.c
@@ -0,0 +1,31 @@
+// Description:Find the trace(sum of diagonal elements) of a matrix
+//Author:Angelina Rose Varghese
+// Date: 17/02/2025
+#include <stdio.h>
+int main()
+{
+    int row,column,i,j,sum=0;
+    printf("Enter the no. of rows and columns: ");
+    scanf("%d%d",&row,&column);
+    int num[row][column];
+    printf("Enter the elements: ");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            scanf("%d",&num[i][j]);
+        }
+    }
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            if(i==j)
+            {
+                sum=sum+num[i][j];
+            }
+        }
+    }
+    printf("The sum of the matrix is %d\n",sum);
+    return 0;
+}
